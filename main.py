765851d644507ec812d80aa390bf9f7d9b76c1ee
Andy Proctor <96781581+Prockie@users.noreply.github.com>
2021-12-28 17:00:55 +0000

    Add table

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import streamlit as st
+import pandas
+
+
+data = {
+  'Series_1':[1,2,3,4,6,9],
+  'Series_2':[10,20,53,250,275,300]
+}
+
+df = pandas.DataFrame(data)
 
 st.title('Our first steamlit app')
 st.subheader('Introducing this as a awy of showing data')
 st.write(''' THis is nice
 Really nice
 ''')
+
+st.write(df)
